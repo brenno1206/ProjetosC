6aab331f096ca4ba67cf44299af72e3cc66b5184
Brenno Gomes <brennogbreda@gmail.com>
2025-08-29 15:46:27 -0300

    Recomecando código do Jogo da Velha

--- 3.JogoDaVelha/main.c
+++ 3.JogoDaVelha/main.c
@@ -8,21 +8,36 @@ char game[SIZE][SIZE];
 void clearScreen();
 void clearGame();
 void makeGame();
+void getMove(char player);
 
 int main() {
-    makeGame();
 
     return 0;
 }
 
+void getMove(char player) {
+    makeGame();
+    printf("\nVez do Jogador %c\n", player);
+
+    int column;
+    int line;
+    printf("Digite a coluna da jogada: ");
+    if(scanf("%d", &column) != 1) {
+        printf("ERRO! O número deve ser inteiro.\nFim do programa");
+        exit(1);
+    } else if(column < 0 || column > 2) {
+        printf("ERRO! Coluna não existente.\nFim do Programa");
+        exit(1);
+    }
+}
 
 void makeGame() {
-    printf("   0   1   2\n");
-    printf("0  %c | %c | %c\n", game[0][0], game[0][1], game[0][2]);
-    printf("  ----+----+-----\n");
-    printf("1  %c | %c | %c\n", game[1][0], game[1][1], game[1][2]);
-    printf("  ----+----+-----\n");
-    printf("2  %c | %c | %c\n", game[2][0], game[2][1], game[2][2]);
+    printf("   0    1    2\n");
+    printf("0  %c  | %c  | %c\n", game[0][0], game[0][1], game[0][2]);
+    printf("  ----+----+----\n");
+    printf("1  %c  | %c  | %c\n", game[1][0], game[1][1], game[1][2]);
+    printf("  ----+----+----\n");
+    printf("2  %c  | %c  | %c\n", game[2][0], game[2][1], game[2][2]);
 
 }
 
